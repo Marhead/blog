f46d9be7c1399748700daa3784bbf6d5f24ed586
송치훈 <marhead@192.168.nate.com>
2022-09-22 13:36:01 +0900

    back to normal

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
-import Image from '@/components/Image'
 import Link from '@/components/Link'
 import { PageSEO } from '@/components/SEO'
 import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import { getAllFilesFrontMatter } from '@/lib/mdx'
 import formatDate from '@/lib/utils/formatDate'
+
+import NewsletterForm from '@/components/NewsletterForm'
 
 const MAX_DISPLAY = 5
 
@@ -17,12 +18,11 @@ export async function getStaticProps() {
 export default function Home({ posts }) {
   return (
     <>
-      <meta name="google-site-verification" content="xshUvQhZHTFjWSKZi7VAvGcegriHhx2TqrnzSWZTnC4" />
       <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Welcome to my Den!
+            Latest
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
             {siteMetadata.description}
@@ -31,20 +31,19 @@ export default function Home({ posts }) {
         <ul className="divide-y divide-gray-200 dark:divide-gray-700">
           {!posts.length && 'No posts found.'}
           {posts.slice(0, MAX_DISPLAY).map((frontMatter) => {
-            const { slug, date, title, summary, tags, images } = frontMatter
+            const { slug, date, title, summary, tags } = frontMatter
             return (
-              <li key={slug} className="py-9">
+              <li key={slug} className="py-12">
                 <article>
-                  <div className="space-y-2 space-x-100 xl:grid xl:grid-cols-5 xl:items-baseline xl:space-y-0">
-                    <div><img alt={title} src={images} className="object-cover w-96"></img></div>
+                  <div className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
+                    <dl>
+                      <dt className="sr-only">Published on</dt>
+                      <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+                        <time dateTime={date}>{formatDate(date)}</time>
+                      </dd>
+                    </dl>
                     <div className="space-y-5 xl:col-span-3">
-                      <div className="space-y-4">
-                        <dl>
-                          <dt className="sr-only">Published on</dt>
-                          <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
-                            <time dateTime={date}>{formatDate(date)}</time>
-                          </dd>
-                        </dl>
+                      <div className="space-y-6">
                         <div>
                           <h2 className="text-2xl font-bold leading-8 tracking-tight">
                             <Link
@@ -90,6 +89,11 @@ export default function Home({ posts }) {
           >
             All Posts &rarr;
           </Link>
+        </div>
+      )}
+      {siteMetadata.newsletter.provider !== '' && (
+        <div className="flex items-center justify-center pt-4">
+          <NewsletterForm />
         </div>
       )}
     </>

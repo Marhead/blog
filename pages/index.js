48cb0637f60011d0e8d3f5ca5afab059ced022c0
송치훈 <marhead@songchihoon-ui-MacBookPro.local>
2022-10-20 03:04:57 +0900

    update class to className

--- pages/index.js
+++ pages/index.js
@@ -30,10 +30,11 @@ export default function Home({ posts }) {
 
         
 
-        <div class="flex h-full bg-white dark:bg-gray-900 rounded overflow-hidden">
-					<a href="post.html" class="flex flex-wrap no-underline hover:no-underline">
+        <div className="flex h-full bg-white dark:bg-gray-900 rounded overflow-hidden">
+					<a href="post.html" className="flex flex-wrap no-underline hover:no-underline">
 
             {!posts.length && 'no posts found.'}
+
             {posts.slice(0, MAX_DISPLAY).map((frontMatter,indexCount)=>{
 
               const {slug, date, title, summary, tags} = frontMatter
@@ -42,26 +43,26 @@ export default function Home({ posts }) {
               if(indexCount == 0) {
                 return(
                   <>
-                  <div class="w-full md:w-2/3 rounded-t">	
-                    <img src="https://source.unsplash.com/collection/494263/800x600" class="h-full w-full shadow"/>
+                  <div className="w-full md:w-2/3 rounded-t">	
+                    <img src="https://source.unsplash.com/collection/494263/800x600" className="h-full w-full shadow"/>
                   </div>
 
-                  <div class="w-full md:w-1/3 flex flex-col flex-grow flex-shrink">
-                    <div class="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow-lg">
-                      <p class="w-full text-gray-600 text-xs md:text-sm pt-6 px-6">
+                  <div className="w-full md:w-1/3 flex flex-col flex-grow flex-shrink">
+                    <div className="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow-lg">
+                      <p className="w-full text-gray-600 text-xs md:text-sm pt-6 px-6">
                         {tags.map((tag) => (
                               <Tag key={tag} text={tag} />
                             ))}</p>
-                      <div class="w-full font-bold text-xl text-gray-900 px-6">{title}</div>
-                      <p class="text-gray-800 text-base px-6 mb-5">
+                      <div className="w-full font-bold text-xl text-gray-900 px-6">{title}</div>
+                      <p className="text-gray-800 text-base px-6 mb-5">
                         {summary}
                       </p>
                     </div>
 
-                    <div class="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow-lg p-6">
-                      <div class="flex items-center justify-between">
-                        <img class="w-8 h-8 rounded-full mr-4 avatar" data-tippy-content="Author Name" src="http://i.pravatar.cc/300" alt="Avatar of Author"/>
-                        <p class="text-gray-600 text-xs md:text-sm"><time dateTime={date}>{formatDate(date)}</time></p>
+                    <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow-lg p-6">
+                      <div className="flex items-center justify-between">
+                        <img className="w-8 h-8 rounded-full mr-4 avatar" data-tippy-content="Author Name" src="http://i.pravatar.cc/300" alt="Avatar of Author"/>
+                        <p className="text-gray-600 text-xs md:text-sm"><time dateTime={date}>{formatDate(date)}</time></p>
                       </div>
                     </div>
                   </div>
@@ -71,25 +72,25 @@ export default function Home({ posts }) {
               if(indexCount == 1 || indexCount == 9) {
                 return(
                 <>
-                <div class="w-full md:w-2/3 p-6 flex flex-col flex-grow flex-shrink">
-                  <div class="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow-lg">
-                    <a href="#" class="flex flex-wrap no-underline hover:no-underline">	
-                      <img src="https://source.unsplash.com/collection/325867/800x600" class="h-full w-full rounded-t pb-6"/>
-                      <p class="w-full text-gray-600 text-xs md:text-sm px-6">
+                <div className="w-full md:w-2/3 p-6 flex flex-col flex-grow flex-shrink">
+                  <div className="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow-lg">
+                    <a href="#" className="flex flex-wrap no-underline hover:no-underline">	
+                      <img src="https://source.unsplash.com/collection/325867/800x600" className="h-full w-full rounded-t pb-6"/>
+                      <p className="w-full text-gray-600 text-xs md:text-sm px-6">
                         {tags.map((tag) => (
                               <Tag key={tag} text={tag} />
                             ))}
                       </p>
-                      <div class="w-full font-bold text-xl text-gray-900 px-6">{title}</div>
-                      <p class="text-gray-800 text-base px-6 mb-5">
+                      <div className="w-full font-bold text-xl text-gray-900 px-6">{title}</div>
+                      <p className="text-gray-800 text-base px-6 mb-5">
                         {summary}
                       </p>
                     </a>
                   </div>
-                  <div class="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow-lg p-6">
-                    <div class="flex items-center justify-between">
-                      <img class="w-8 h-8 rounded-full mr-4 avatar" data-tippy-content="Author Name" src="http://i.pravatar.cc/300" alt="Avatar of Author"/>
-                      <p class="text-gray-600 text-xs md:text-sm"><time dateTime={date}>{formatDate(date)}</time></p>
+                  <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow-lg p-6">
+                    <div className="flex items-center justify-between">
+                      <img className="w-8 h-8 rounded-full mr-4 avatar" data-tippy-content="Author Name" src="http://i.pravatar.cc/300" alt="Avatar of Author"/>
+                      <p className="text-gray-600 text-xs md:text-sm"><time dateTime={date}>{formatDate(date)}</time></p>
                     </div>
                   </div>
                 </div>
@@ -100,25 +101,25 @@ export default function Home({ posts }) {
               if(indexCount == 2 || indexCount == 3 || indexCount == 4 || indexCount == 5 || indexCount == 8) {
                 return(
                 <>
-                <div class="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
-                  <div class="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow-lg">
-                    <a href="#" class="flex flex-wrap no-underline hover:no-underline">
-                      <img src="https://source.unsplash.com/collection/1118905/800x600" class="h-full w-full rounded-t pb-6"/>
-                      <p class="w-full text-gray-600 text-xs md:text-sm px-6">
+                <div className="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
+                  <div className="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow-lg">
+                    <a href="#" className="flex flex-wrap no-underline hover:no-underline">
+                      <img src="https://source.unsplash.com/collection/1118905/800x600" className="h-full w-full rounded-t pb-6"/>
+                      <p className="w-full text-gray-600 text-xs md:text-sm px-6">
                       {tags.map((tag) => (
                               <Tag key={tag} text={tag} />
                             ))}
                       </p>
-                      <div class="w-full font-bold text-xl text-gray-900 px-6">{title}</div>
-                      <p class="text-gray-800 text-base px-6 mb-5">
+                      <div className="w-full font-bold text-xl text-gray-900 px-6">{title}</div>
+                      <p className="text-gray-800 text-base px-6 mb-5">
                         {summary} 
                       </p>
                     </a>
                   </div>
-                  <div class="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow-lg p-6">
-                    <div class="flex items-center justify-between">
-                      <img class="w-8 h-8 rounded-full mr-4 avatar" data-tippy-content="Author Name" src="http://i.pravatar.cc/300" alt="Avatar of Author"/>
-                      <p class="text-gray-600 text-xs md:text-sm"><time dateTime={date}>{formatDate(date)}</time></p>
+                  <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow-lg p-6">
+                    <div className="flex items-center justify-between">
+                      <img className="w-8 h-8 rounded-full mr-4 avatar" data-tippy-content="Author Name" src="http://i.pravatar.cc/300" alt="Avatar of Author"/>
+                      <p className="text-gray-600 text-xs md:text-sm"><time dateTime={date}>{formatDate(date)}</time></p>
                     </div>
                   </div>
                 </div>
@@ -128,25 +129,25 @@ export default function Home({ posts }) {
               if(indexCount == 6 || indexCount == 7) {
                 return(
                 <>
-                <div class="w-full md:w-1/2 p-6 flex flex-col flex-grow flex-shrink">
-                  <div class="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow-lg">
-                    <a href="#" class="flex flex-wrap no-underline hover:no-underline">
-                      <img src="https://source.unsplash.com/collection/3657445/800x600" class="h-full w-full rounded-t pb-6"/>
-                      <p class="w-full text-gray-600 text-xs md:text-sm px-6">
+                <div className="w-full md:w-1/2 p-6 flex flex-col flex-grow flex-shrink">
+                  <div className="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow-lg">
+                    <a href="#" className="flex flex-wrap no-underline hover:no-underline">
+                      <img src="https://source.unsplash.com/collection/3657445/800x600" className="h-full w-full rounded-t pb-6"/>
+                      <p className="w-full text-gray-600 text-xs md:text-sm px-6">
                       {tags.map((tag) => (
                               <Tag key={tag} text={tag} />
                             ))}
                       </p>
-                      <div class="w-full font-bold text-xl text-gray-900 px-6">{title}</div>
-                      <p class="text-gray-800 text-base px-6 mb-5">
+                      <div className="w-full font-bold text-xl text-gray-900 px-6">{title}</div>
+                      <p className="text-gray-800 text-base px-6 mb-5">
                         {summary} 
                       </p>
                     </a>
                   </div>
-                  <div class="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow-lg p-6">
-                    <div class="flex items-center justify-between">
-                      <img class="w-8 h-8 rounded-full mr-4 avatar" data-tippy-content="Author Name" src="http://i.pravatar.cc/300" alt="Avatar of Author"/>
-                      <p class="text-gray-600 text-xs md:text-sm"><time dateTime={date}>{formatDate(date)}</time></p>
+                  <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow-lg p-6">
+                    <div className="flex items-center justify-between">
+                      <img className="w-8 h-8 rounded-full mr-4 avatar" data-tippy-content="Author Name" src="http://i.pravatar.cc/300" alt="Avatar of Author"/>
+                      <p className="text-gray-600 text-xs md:text-sm"><time dateTime={date}>{formatDate(date)}</time></p>
                     </div>
                   </div>
                 </div>
@@ -155,7 +156,7 @@ export default function Home({ posts }) {
               }
             })}
 
-				    <div class="flex flex-wrap justify-between pt-12 -mx-6">
+				    <div className="flex flex-wrap justify-between pt-12 -mx-6">
             </div>
 					</a>
 				</div>

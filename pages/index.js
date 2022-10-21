267c185c463ba4c9dd0fb96edb752ef359708db3
송치훈 <marhead@songchihoon-ui-MacBookPro.local>
2022-10-21 11:37:54 +0900

    change code into prettier

--- pages/index.js
+++ pages/index.js
@@ -27,26 +27,18 @@ export default function Home({ posts }) {
             {siteMetadata.description}
           </p>
         </div>
-
-        
-
-        <div className="flex h-full bg-white dark:bg-gray-900 rounded overflow-hidden">
+        <div className="flex h-full overflow-hidden rounded bg-white dark:bg-gray-900">
 					<a href="post.html" className="flex flex-wrap no-underline hover:no-underline">
-
             {!posts.length && 'no posts found.'}
-
             {posts.slice(0, MAX_DISPLAY).map((frontMatter,indexCount)=>{
-
-              const {slug, date, title, summary, tags} = frontMatter
-
+              const { slug, date, title, summary, tags } = frontMatter
               // 대문
               if(indexCount == 0) {
                 return(
                   <>
-                  <div className="w-full md:w-2/3 rounded-t">	
-                    <img src="https://source.unsplash.com/collection/494263/800x600" className="h-full w-full shadow"/>
+                  <div key={slug} className="w-full rounded-t md:w-2/3">	
+                    <Image src="https://source.unsplash.com/collection/494263/800x600" className="h-full w-full shadow"/>
                   </div>
-
                   <div className="w-full md:w-1/3 flex flex-col flex-grow flex-shrink">
                     <div className="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow-lg">
                       <p className="w-full text-gray-600 text-xs md:text-sm pt-6 px-6">
@@ -61,7 +53,7 @@ export default function Home({ posts }) {
 
                     <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow-lg p-6">
                       <div className="flex items-center justify-between">
-                        <img className="w-8 h-8 rounded-full mr-4 avatar" data-tippy-content="Author Name" src="http://i.pravatar.cc/300" alt="Avatar of Author"/>
+                        <Image className="w-8 h-8 rounded-full mr-4 avatar" data-tippy-content="Author Name" src="http://i.pravatar.cc/300" alt="Avatar of Author"/>
                         <p className="text-gray-600 text-xs md:text-sm"><time dateTime={date}>{formatDate(date)}</time></p>
                       </div>
                     </div>
@@ -72,10 +64,10 @@ export default function Home({ posts }) {
               if(indexCount == 1 || indexCount == 9) {
                 return(
                 <>
-                <div className="w-full md:w-2/3 p-6 flex flex-col flex-grow flex-shrink">
+                <div key={slug} className="w-full md:w-2/3 p-6 flex flex-col flex-grow flex-shrink">
                   <div className="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow-lg">
                     <a href="#" className="flex flex-wrap no-underline hover:no-underline">	
-                      <img src="https://source.unsplash.com/collection/325867/800x600" className="h-full w-full rounded-t pb-6"/>
+                      <Image src="https://source.unsplash.com/collection/325867/800x600" className="h-full w-full rounded-t pb-6"/>
                       <p className="w-full text-gray-600 text-xs md:text-sm px-6">
                         {tags.map((tag) => (
                               <Tag key={tag} text={tag} />
@@ -89,7 +81,7 @@ export default function Home({ posts }) {
                   </div>
                   <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow-lg p-6">
                     <div className="flex items-center justify-between">
-                      <img className="w-8 h-8 rounded-full mr-4 avatar" data-tippy-content="Author Name" src="http://i.pravatar.cc/300" alt="Avatar of Author"/>
+                      <Image className="w-8 h-8 rounded-full mr-4 avatar" data-tippy-content="Author Name" src="http://i.pravatar.cc/300" alt="Avatar of Author"/>
                       <p className="text-gray-600 text-xs md:text-sm"><time dateTime={date}>{formatDate(date)}</time></p>
                     </div>
                   </div>
@@ -101,10 +93,10 @@ export default function Home({ posts }) {
               if(indexCount == 2 || indexCount == 3 || indexCount == 4 || indexCount == 5 || indexCount == 8) {
                 return(
                 <>
-                <div className="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
+                <div key={slug} className="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
                   <div className="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow-lg">
                     <a href="#" className="flex flex-wrap no-underline hover:no-underline">
-                      <img src="https://source.unsplash.com/collection/1118905/800x600" className="h-full w-full rounded-t pb-6"/>
+                      <Image src="https://source.unsplash.com/collection/1118905/800x600" className="h-full w-full rounded-t pb-6"/>
                       <p className="w-full text-gray-600 text-xs md:text-sm px-6">
                       {tags.map((tag) => (
                               <Tag key={tag} text={tag} />
@@ -118,7 +110,7 @@ export default function Home({ posts }) {
                   </div>
                   <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow-lg p-6">
                     <div className="flex items-center justify-between">
-                      <img className="w-8 h-8 rounded-full mr-4 avatar" data-tippy-content="Author Name" src="http://i.pravatar.cc/300" alt="Avatar of Author"/>
+                      <Image className="w-8 h-8 rounded-full mr-4 avatar" data-tippy-content="Author Name" src="http://i.pravatar.cc/300" alt="Avatar of Author"/>
                       <p className="text-gray-600 text-xs md:text-sm"><time dateTime={date}>{formatDate(date)}</time></p>
                     </div>
                   </div>
@@ -129,10 +121,10 @@ export default function Home({ posts }) {
               if(indexCount == 6 || indexCount == 7) {
                 return(
                 <>
-                <div className="w-full md:w-1/2 p-6 flex flex-col flex-grow flex-shrink">
+                <div key={slug} className="w-full md:w-1/2 p-6 flex flex-col flex-grow flex-shrink">
                   <div className="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow-lg">
                     <a href="#" className="flex flex-wrap no-underline hover:no-underline">
-                      <img src="https://source.unsplash.com/collection/3657445/800x600" className="h-full w-full rounded-t pb-6"/>
+                      <Image src="https://source.unsplash.com/collection/3657445/800x600" className="h-full w-full rounded-t pb-6"/>
                       <p className="w-full text-gray-600 text-xs md:text-sm px-6">
                       {tags.map((tag) => (
                               <Tag key={tag} text={tag} />
@@ -146,7 +138,7 @@ export default function Home({ posts }) {
                   </div>
                   <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow-lg p-6">
                     <div className="flex items-center justify-between">
-                      <img className="w-8 h-8 rounded-full mr-4 avatar" data-tippy-content="Author Name" src="http://i.pravatar.cc/300" alt="Avatar of Author"/>
+                      <Image className="w-8 h-8 rounded-full mr-4 avatar" data-tippy-content="Author Name" src="http://i.pravatar.cc/300" alt="Avatar of Author"/>
                       <p className="text-gray-600 text-xs md:text-sm"><time dateTime={date}>{formatDate(date)}</time></p>
                     </div>
                   </div>

cc47d309819812f3bdb369c0dca385a60dcaae21
송치훈 <marhead@songchihoon-ui-MacBookPro.local>
2022-10-24 08:23:00 +0900

    update for desktop programming

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Link from '@/components/Link'
 import { PageSEO } from '@/components/SEO'
 import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
+import Image from '@/components/Image'
 import { getAllFilesFrontMatter } from '@/lib/mdx'
 import formatDate from '@/lib/utils/formatDate'
 import { slug } from 'github-slugger'
@@ -28,32 +29,46 @@ export default function Home({ posts }) {
           </p>
         </div>
         <div className="flex h-full overflow-hidden rounded bg-white dark:bg-gray-900">
-					<a href="post.html" className="flex flex-wrap no-underline hover:no-underline">
+					<a className="flex flex-wrap no-underline hover:no-underline position: relative">
             {!posts.length && 'no posts found.'}
-            {posts.slice(0, MAX_DISPLAY).map((frontMatter,indexCount)=>{
+            {posts.slice(0, MAX_DISPLAY).map((frontMatter,indexCount) =>{
               const { slug, date, title, summary, tags } = frontMatter
               // 대문
-              if(indexCount == 0) {
+              if (indexCount == 0) {
                 return(
                   <>
-                  <div key={slug} className="w-full rounded-t md:w-2/3">	
-                    <Image src="https://source.unsplash.com/collection/494263/800x600" className="h-full w-full shadow"/>
+                  <div key={slug} className="w-full rounded-t md:w-2/3">
+                    <Image 
+                      src={"/laptopwithdesk.jpg"}
+                      layout="fill"
+                      className="h-full w-full shadow"
+                      href={"/blog/${slug}"}/>
                   </div>
-                  <div className="w-full md:w-1/3 flex flex-col flex-grow flex-shrink">
-                    <div className="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow-lg">
-                      <p className="w-full text-gray-600 text-xs md:text-sm pt-6 px-6">
-                        {tags.map((tag) => (
-                              <Tag key={tag} text={tag} />
-                            ))}</p>
-                      <div className="w-full font-bold text-xl text-gray-900 px-6">{title}</div>
-                      <p className="text-gray-800 text-base px-6 mb-5">
-                        {summary}
+                  <div className="flex w-full flex-shrink flex-grow flex-col md:w-1/3">
+                    <div className="flex-1 overflow-hidden rounded-t rounded-b-none bg-white shadow-lg">
+                      <p className="w-full px-6 pt-6 text-xs text-gray-600 md:text-sm "> 
+                        {tags.map((tag) => ( 
+                              <Tag key={tag} text={tag} /> 
+                            ))}
+
+                            </p>
+                      <div className="w-full px-6 text-xl font-bold text-gray-900">
+                        <a href={"/blog/${slug}"}>{title}</a></div>
+                      <p className="mb-5 px-6 text-base text-gray-800">
+                        {summary} 
                       </p>
                     </div>
 
-                    <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow-lg p-6">
-                      <div className="flex items-center justify-between">
-                        <Image className="w-8 h-8 rounded-full mr-4 avatar" data-tippy-content="Author Name" src="http://i.pravatar.cc/300" alt="Avatar of Author"/>
+                    <div className="mt-auto flex-none bg-white rounded-b rounded-t-none overflow-hidden shadow-lg p-6">
+                      <div className="flex items-center justify-between position: relative">
+                        <Image 
+                          src="/static/images/avatar.jpg"
+                          alt="Avatar"
+                          width="32px"
+                          height="32px"
+                          className="w-8 h-8 rounded-full mr-4 avatar" 
+                          data-tippy-content="Marhead" 
+                          />
                         <p className="text-gray-600 text-xs md:text-sm"><time dateTime={date}>{formatDate(date)}</time></p>
                       </div>
                     </div>
@@ -66,8 +81,11 @@ export default function Home({ posts }) {
                 <>
                 <div key={slug} className="w-full md:w-2/3 p-6 flex flex-col flex-grow flex-shrink">
                   <div className="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow-lg">
-                    <a href="#" className="flex flex-wrap no-underline hover:no-underline">	
-                      <Image src="https://source.unsplash.com/collection/325867/800x600" className="h-full w-full rounded-t pb-6"/>
+                    <a href="#" className="flex flex-wrap no-underline hover:no-underline position: relative">	
+                      <Image 
+                        src={"/laptopwithdesk.jpg"} 
+                        layout="fill"
+                        className="h-full w-full rounded-t pb-6"/>
                       <p className="w-full text-gray-600 text-xs md:text-sm px-6">
                         {tags.map((tag) => (
                               <Tag key={tag} text={tag} />
@@ -80,8 +98,14 @@ export default function Home({ posts }) {
                     </a>
                   </div>
                   <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow-lg p-6">
-                    <div className="flex items-center justify-between">
-                      <Image className="w-8 h-8 rounded-full mr-4 avatar" data-tippy-content="Author Name" src="http://i.pravatar.cc/300" alt="Avatar of Author"/>
+                    <div className="flex items-center justify-between position: relative">
+                      <Image 
+                        className="w-8 h-8 rounded-full mr-4 avatar" 
+                        data-tippy-content="Author Name" 
+                        src="/static/images/avatar.jpg" 
+                        width="32px"
+                        height="32px"
+                        alt="Avatar of Author"/>
                       <p className="text-gray-600 text-xs md:text-sm"><time dateTime={date}>{formatDate(date)}</time></p>
                     </div>
                   </div>
@@ -95,8 +119,11 @@ export default function Home({ posts }) {
                 <>
                 <div key={slug} className="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
                   <div className="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow-lg">
-                    <a href="#" className="flex flex-wrap no-underline hover:no-underline">
-                      <Image src="https://source.unsplash.com/collection/1118905/800x600" className="h-full w-full rounded-t pb-6"/>
+                    <a href="#" className="flex flex-wrap no-underline hover:no-underline position: relative">
+                      <Image 
+                        src={"/laptopwithdesk.jpg"}
+                        layout="fill" 
+                        className="h-full w-full rounded-t pb-6"/>
                       <p className="w-full text-gray-600 text-xs md:text-sm px-6">
                       {tags.map((tag) => (
                               <Tag key={tag} text={tag} />
@@ -109,8 +136,14 @@ export default function Home({ posts }) {
                     </a>
                   </div>
                   <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow-lg p-6">
-                    <div className="flex items-center justify-between">
-                      <Image className="w-8 h-8 rounded-full mr-4 avatar" data-tippy-content="Author Name" src="http://i.pravatar.cc/300" alt="Avatar of Author"/>
+                    <div className="flex items-center justify-between position:relative">
+                      <Image 
+                        className="w-8 h-8 rounded-full mr-4 avatar" 
+                        data-tippy-content="Author Name" 
+                        width="32px"
+                        height="32px"
+                        src="/static/images/avatar.jpg" 
+                        alt="Avatar of Author"/>
                       <p className="text-gray-600 text-xs md:text-sm"><time dateTime={date}>{formatDate(date)}</time></p>
                     </div>
                   </div>
@@ -123,8 +156,11 @@ export default function Home({ posts }) {
                 <>
                 <div key={slug} className="w-full md:w-1/2 p-6 flex flex-col flex-grow flex-shrink">
                   <div className="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow-lg">
-                    <a href="#" className="flex flex-wrap no-underline hover:no-underline">
-                      <Image src="https://source.unsplash.com/collection/3657445/800x600" className="h-full w-full rounded-t pb-6"/>
+                    <a href="#" className="flex flex-wrap no-underline hover:no-underline position: relative">
+                      <Image 
+                        src={"/laptopwithdesk.jpg"} 
+                        layout="fill"
+                        className="h-full w-full rounded-t pb-6"/>
                       <p className="w-full text-gray-600 text-xs md:text-sm px-6">
                       {tags.map((tag) => (
                               <Tag key={tag} text={tag} />
@@ -137,8 +173,14 @@ export default function Home({ posts }) {
                     </a>
                   </div>
                   <div className="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow-lg p-6">
-                    <div className="flex items-center justify-between">
-                      <Image className="w-8 h-8 rounded-full mr-4 avatar" data-tippy-content="Author Name" src="http://i.pravatar.cc/300" alt="Avatar of Author"/>
+                    <div className="flex items-center justify-between position: relative">
+                      <Image 
+                        className="w-8 h-8 rounded-full mr-4 avatar" 
+                        data-tippy-content="Author Name"
+                        src="/static/images/avatar.jpg" 
+                        width="32px"
+                        height="32px"
+                        alt="Avatar of Author"/>
                       <p className="text-gray-600 text-xs md:text-sm"><time dateTime={date}>{formatDate(date)}</time></p>
                     </div>
                   </div>

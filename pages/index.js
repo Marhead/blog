f609f1ca4628e4c6771686a994e69906f52342e3
송치훈 <marhead@songchihoon-ui-MacBookPro.local>
2022-10-25 17:30:24 +0900

    reorganize code

--- pages/index.js
+++ pages/index.js
@@ -30,8 +30,8 @@ export default function Home({ posts }) {
         </div>
         <div className="flex h-full overflow-hidden rounded bg-white dark:bg-gray-900">
           <a className="position: relative flex flex-wrap no-underline hover:no-underline">
-			        <div class="mx-0 sm:mx-6">
-                <div class="bg-white dark:bg-gray-900 w-full text-xl md:text-2xl text-gray-800 leading-normal rounded-t">
+              <div className="mx-0 sm:mx-6">
+                <div className="w-full rounded-t bg-white text-xl leading-normal text-gray-800 dark:bg-gray-900 md:text-2xl">
                 {!posts.length && 'no posts found.'}
                 {posts.slice(0, 1).map((frontMatter, indexCount) => {
                   const { slug, date, title, summary, tags, image } = frontMatter
@@ -39,8 +39,8 @@ export default function Home({ posts }) {
                   if (indexCount == 0) {
                     return (
                       <>
-                        <div class="flex h-full bg-white rounded overflow-hidden shadow-lg">
-                          <a href={`/blog/${slug}`} class="flex flex-wrap no-underline hover:no-underline">
+                        <div className="flex h-full bg-white rounded overflow-hidden shadow-lg">
+                          <a href={`/blog/${slug}`} className="flex flex-wrap no-underline hover:no-underline">
                             <div key={slug} className="w-full rounded-t md:w-2/3">
                               <Image
                                 src={`/static/images/${image}`}
@@ -84,166 +84,166 @@ export default function Home({ posts }) {
                   }
                 })}
                 <div class="flex flex-wrap justify-between pt-12 -mx-6">
-                {posts.slice(1, MAX_DISPLAY).map((frontMatter, indexCount) => {
-                  const { slug, date, title, summary, tags, image } = frontMatter
+                  {posts.slice(1, MAX_DISPLAY).map((frontMatter, indexCount) => {
+                    const { slug, date, title, summary, tags, image } = frontMatter
 
-                  if (indexCount == 1 || indexCount == 9) {
-                    return (
-                      <>
-                        <div
-                          key={slug}
-                          className="flex w-full flex-shrink flex-grow flex-col p-6 md:w-2/3"
-                        >
-                          <div className="flex-1 overflow-hidden rounded-t rounded-b-none bg-gray-100 shadow-lg">
-                            <a
-                              href="#"
-                              className="position: relative flex flex-wrap no-underline hover:no-underline"
-                            >
-                              <Link href={`/blog/${slug}`}>
+                    if (indexCount == 1 || indexCount == 9) {
+                      return (
+                        <>
+                          <div
+                            key={slug}
+                            className="flex w-full flex-shrink flex-grow flex-col p-6 md:w-2/3"
+                          >
+                            <div className="flex-1 overflow-hidden rounded-t rounded-b-none bg-gray-100 shadow-lg">
+                              <a
+                                href="#"
+                                className="position: relative flex flex-wrap no-underline hover:no-underline"
+                              >
+                                <Link href={`/blog/${slug}`}>
+                                  <Image
+                                    src={`/static/images/${image}`}
+                                    width="800"
+                                    height="600"
+                                    className="h-full w-full rounded-t pb-6"
+                                  />
+                                </Link>
+                                <p className="w-full px-6 text-xs text-gray-600 md:text-sm">
+                                  {tags.map((tag) => (
+                                    <Tag key={tag} text={tag} />
+                                  ))}
+                                </p>
+                                <div className="w-full px-6 text-xl font-bold text-gray-900">
+                                  <Link href={`/blog/${slug}`}>{title}</Link>
+                                </div>
+                                <p className="mb-5 px-6 text-base text-gray-800">{summary}</p>
+                              </a>
+                            </div>
+                            <div className="mt-auto flex-none overflow-hidden rounded-b rounded-t-none bg-gray-100 p-6 shadow-lg">
+                              <div className="position: relative flex items-center justify-between">
                                 <Image
-                                  src={`/static/images/${image}`}
-                                  width="800"
-                                  height="600"
-                                  className="h-full w-full rounded-t pb-6"
+                                  className="avatar mr-4 h-8 w-8 rounded-full"
+                                  data-tippy-content="Author Name"
+                                  src="/static/images/avatar.jpg"
+                                  width="32px"
+                                  height="32px"
+                                  alt="Avatar of Author"
                                 />
-                              </Link>
-                              <p className="w-full px-6 text-xs text-gray-600 md:text-sm">
-                                {tags.map((tag) => (
-                                  <Tag key={tag} text={tag} />
-                                ))}
-                              </p>
-                              <div className="w-full px-6 text-xl font-bold text-gray-900">
-                                <Link href={`/blog/${slug}`}>{title}</Link>
+                                <p className="text-xs text-gray-600 md:text-sm">
+                                  <time dateTime={date}>{formatDate(date)}</time>
+                                </p>
                               </div>
-                              <p className="mb-5 px-6 text-base text-gray-800">{summary}</p>
-                            </a>
-                          </div>
-                          <div className="mt-auto flex-none overflow-hidden rounded-b rounded-t-none bg-gray-100 p-6 shadow-lg">
-                            <div className="position: relative flex items-center justify-between">
-                              <Image
-                                className="avatar mr-4 h-8 w-8 rounded-full"
-                                data-tippy-content="Author Name"
-                                src="/static/images/avatar.jpg"
-                                width="32px"
-                                height="32px"
-                                alt="Avatar of Author"
-                              />
-                              <p className="text-xs text-gray-600 md:text-sm">
-                                <time dateTime={date}>{formatDate(date)}</time>
-                              </p>
                             </div>
                           </div>
-                        </div>
-                      </>
-                    )
-                  }
-                  if (
-                    indexCount == 2 ||
-                    indexCount == 3 ||
-                    indexCount == 4 ||
-                    indexCount == 5 ||
-                    indexCount == 8
-                  ) {
-                    return (
-                      <>
-                        <div
-                          key={slug}
-                          className="flex w-full flex-shrink flex-grow flex-col p-6 md:w-1/3"
-                        >
-                          <div className="flex-1 overflow-hidden rounded-t rounded-b-none bg-gray-100 shadow-lg">
-                            <a
-                              href="#"
-                              className="position: relative flex flex-wrap no-underline hover:no-underline"
-                            >
-                              <Link href={`/blog/${slug}`}>
+                        </>
+                      )
+                    }
+                    if (
+                      indexCount == 2 ||
+                      indexCount == 3 ||
+                      indexCount == 4 ||
+                      indexCount == 5 ||
+                      indexCount == 8
+                    ) {
+                      return (
+                        <>
+                          <div
+                            key={slug}
+                            className="flex w-full flex-shrink flex-grow flex-col p-6 md:w-1/3"
+                          >
+                            <div className="flex-1 overflow-hidden rounded-t rounded-b-none bg-gray-100 shadow-lg">
+                              <a
+                                href="#"
+                                className="position: relative flex flex-wrap no-underline hover:no-underline"
+                              >
+                                <Link href={`/blog/${slug}`}>
+                                  <Image
+                                    src={`/static/images/${image}`}
+                                    width="800"
+                                    height="600"
+                                    className="h-full w-full rounded-t pb-6"
+                                  />
+                                </Link>
+                                <p className="w-full px-6 text-xs text-gray-600 md:text-sm">
+                                  {tags.map((tag) => (
+                                    <Tag key={tag} text={tag} />
+                                  ))}
+                                </p>
+                                <div className="w-full px-6 text-xl font-bold text-gray-900">
+                                  <Link href={`/blog/${slug}`}>{title}</Link>
+                                </div>
+                                <p className="mb-5 px-6 text-base text-gray-800">{summary}</p>
+                              </a>
+                            </div>
+                            <div className="mt-auto flex-none overflow-hidden rounded-b rounded-t-none bg-gray-100 p-6 shadow-lg">
+                              <div className="position: relative flex items-center justify-between">
                                 <Image
-                                  src={`/static/images/${image}`}
-                                  width="800"
-                                  height="600"
-                                  className="h-full w-full rounded-t pb-6"
+                                  className="avatar mr-4 h-8 w-8 rounded-full"
+                                  data-tippy-content="Author Name"
+                                  width="32px"
+                                  height="32px"
+                                  src="/static/images/avatar.jpg"
+                                  alt="Avatar of Author"
                                 />
-                              </Link>
-                              <p className="w-full px-6 text-xs text-gray-600 md:text-sm">
-                                {tags.map((tag) => (
-                                  <Tag key={tag} text={tag} />
-                                ))}
-                              </p>
-                              <div className="w-full px-6 text-xl font-bold text-gray-900">
-                                <Link href={`/blog/${slug}`}>{title}</Link>
+                                <p className="text-xs text-gray-600 md:text-sm">
+                                  <time dateTime={date}>{formatDate(date)}</time>
+                                </p>
                               </div>
-                              <p className="mb-5 px-6 text-base text-gray-800">{summary}</p>
-                            </a>
-                          </div>
-                          <div className="mt-auto flex-none overflow-hidden rounded-b rounded-t-none bg-gray-100 p-6 shadow-lg">
-                            <div className="position: relative flex items-center justify-between">
-                              <Image
-                                className="avatar mr-4 h-8 w-8 rounded-full"
-                                data-tippy-content="Author Name"
-                                width="32px"
-                                height="32px"
-                                src="/static/images/avatar.jpg"
-                                alt="Avatar of Author"
-                              />
-                              <p className="text-xs text-gray-600 md:text-sm">
-                                <time dateTime={date}>{formatDate(date)}</time>
-                              </p>
                             </div>
                           </div>
-                        </div>
-                      </>
-                    )
-                  }
-                  if (indexCount == 6 || indexCount == 7) {
-                    return (
-                      <>
-                        <div
-                          key={slug}
-                          className="flex w-full flex-shrink flex-grow flex-col p-6 md:w-1/2"
-                        >
-                          <div className="flex-1 overflow-hidden rounded-t rounded-b-none bg-gray-100 shadow-lg">
-                            <a
-                              href="#"
-                              className="position: relative flex flex-wrap no-underline hover:no-underline"
-                            >
-                              <Link href={`/blog/${slug}`}>
+                        </>
+                      )
+                    }
+                    if (indexCount == 6 || indexCount == 7) {
+                      return (
+                        <>
+                          <div
+                            key={slug}
+                            className="flex w-full flex-shrink flex-grow flex-col p-6 md:w-1/2"
+                          >
+                            <div className="flex-1 overflow-hidden rounded-t rounded-b-none bg-gray-100 shadow-lg">
+                              <a
+                                href="#"
+                                className="position: relative flex flex-wrap no-underline hover:no-underline"
+                              >
+                                <Link href={`/blog/${slug}`}>
+                                  <Image
+                                    src={`/static/images/${image}`}
+                                    width="800"
+                                    height="600"
+                                    className="h-full w-full rounded-t pb-6"
+                                  />
+                                </Link>
+                                <p className="w-full px-6 text-xs text-gray-600 md:text-sm">
+                                  {tags.map((tag) => (
+                                    <Tag key={tag} text={tag} />
+                                  ))}
+                                </p>
+                                <div className="w-full px-6 text-xl font-bold text-gray-900">
+                                  <Link href={`/blog/${slug}`}>{title}</Link>
+                                </div>
+                                <p className="mb-5 px-6 text-base text-gray-800">{summary}</p>
+                              </a>
+                            </div>
+                            <div className="mt-auto flex-none overflow-hidden rounded-b rounded-t-none bg-gray-100 p-6 shadow-lg">
+                              <div className="position: relative flex items-center justify-between">
                                 <Image
-                                  src={`/static/images/${image}`}
-                                  width="800"
-                                  height="600"
-                                  className="h-full w-full rounded-t pb-6"
+                                  className="avatar mr-4 h-8 w-8 rounded-full"
+                                  data-tippy-content="Author Name"
+                                  src="/static/images/avatar.jpg"
+                                  width="32px"
+                                  height="32px"
+                                  alt="Avatar of Author"
                                 />
-                              </Link>
-                              <p className="w-full px-6 text-xs text-gray-600 md:text-sm">
-                                {tags.map((tag) => (
-                                  <Tag key={tag} text={tag} />
-                                ))}
-                              </p>
-                              <div className="w-full px-6 text-xl font-bold text-gray-900">
-                                <Link href={`/blog/${slug}`}>{title}</Link>
+                                <p className="text-xs text-gray-600 md:text-sm">
+                                  <time dateTime={date}>{formatDate(date)}</time>
+                                </p>
                               </div>
-                              <p className="mb-5 px-6 text-base text-gray-800">{summary}</p>
-                            </a>
-                          </div>
-                          <div className="mt-auto flex-none overflow-hidden rounded-b rounded-t-none bg-gray-100 p-6 shadow-lg">
-                            <div className="position: relative flex items-center justify-between">
-                              <Image
-                                className="avatar mr-4 h-8 w-8 rounded-full"
-                                data-tippy-content="Author Name"
-                                src="/static/images/avatar.jpg"
-                                width="32px"
-                                height="32px"
-                                alt="Avatar of Author"
-                              />
-                              <p className="text-xs text-gray-600 md:text-sm">
-                                <time dateTime={date}>{formatDate(date)}</time>
-                              </p>
                             </div>
                           </div>
-                        </div>
-                      </>
-                    )
-                  }
-                })}
+                        </>
+                      )
+                    }
+                  })}
                 </div>
                 </div>
               </div>
